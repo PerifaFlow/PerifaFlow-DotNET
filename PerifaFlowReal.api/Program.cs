e5728bb4616547c881d1c48a0d4dcc61ce436aab
victorhugo55 <davichugopereira@gmail.com>
2025-11-21 20:00:30 -0300

    Feat: NewProject

---
+++ PerifaFlowReal.api/Program.cs
@@ -0,0 +1,42 @@
+using PerifaFlowReal.Application.Configs;
+using PerifaFlowReal.Infastructure;
+
+namespace PerifaFlowReal.api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var configs = builder.Configuration.Get<Settings>();
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        builder.Services.AddInfra(configs);
+        
+        var app = builder.Build();
+        
+        
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
